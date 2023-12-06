9300f917286a9391c4f34e272a7cecf5a4c7e3a7
wid100 <helal.womenindigital@gmail.com>
2023-12-06 12:43:11 +0600

    update

--- src/components/section/HeroSlider.js
+++ src/components/section/HeroSlider.js
@@ -108,7 +108,7 @@ const HeroSlider = () => {
                               class="form-select"
                               aria-label="Default select example"
                             >
-                              <option selected>Open this select menu</option>
+                              <option selected>Bangladesh - BD</option>
                               <option value="1">One</option>
                               <option value="2">Two</option>
                               <option value="3">Three</option>
